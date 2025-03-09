b90c75f541cca956ca1b95d99a1d04715b1426ad
ales <rightbergs@gmail.com>
2025-03-09 20:00:36 +0300

    add update func for collector nodes to gopc ua server

--- plugin.go
+++ plugin.go
@@ -21,15 +21,53 @@ type Device struct {
 
 type Config struct {
 	CollectorPath string   `json:"collector path"`
-	TimeOut       float32  `json:"timeout"`
+	UpdateTime    float32  `json:"timeout"`
 	Server        Server   `json:"server"`
 	Devices       []Device `json:"devices"`
 }
 
-type DeviceData struct {
-	ProgName string
-	ProgNum  int
+type ModeData struct {
+	Mode       string `json:"mode"`
+	RunState   string `json:"run_state"`
+	Status     string `json:"status"`
+	Shutdowns  string `json:"shutdowns"`
+	HightSpeed string `json:"hight_speed"`
+	AxisMotion string `json:"axis_motion"`
+	Mstb       string `json:"mstb"`
+	LoadExcess string `json:"load_excess"`
 }
+
+type ProgramData struct {
+	Frame          string `json:"frame"`
+	MainProgNumber int    `json:"main_prog_number"`
+	SubProgNumber  int    `json:"sub_prog_number"`
+	PartsCount     int    `json:"parts_count"`
+	ToolNumber     int    `json:"tool_number"`
+	FrameNumber    int    `json:"frame_number"`
+}
+
+type AxesData struct {
+	FeedRate           int            `json:"feedrate"`
+	FeedOverride       int            `json:"feed_override"`
+	JogOverride        float64        `json:"jog_override"`
+	JogSpeed           int            `json:"jog_speed"`
+	CurrentLoad        float64        `json:"current_load"`
+	CurrentLoadPercent float64        `json:"current_load_percent"`
+	ServoLoads         map[string]int `json:"servo_loads"`
+}
+
+type CollectorData struct {
+	Device  Device      `json:"device"`
+	Mode    ModeData    `json:"mode_data"`
+	Program ProgramData `json:"program_data"`
+	Axes    AxesData    `json:"axes_data"`
+}
+
+type CollectorsData struct {
+	Collectors []CollectorData `json:"collectors"`
+}
+
+var collectors_data CollectorsData
 
 func main() {
 	buffer, err := os.ReadFile("plugin.json")
@@ -46,8 +84,10 @@ func main() {
 	}
 
 	if config.Server.Status {
+		device_count := len(config.Devices)
+		collectors_data.Collectors = make([]CollectorData, device_count)
 		inicialize()
-		go launch()
+		go start()
 	}
 
 	collector_path := config.CollectorPath
@@ -66,8 +106,16 @@ func main() {
 			panic(err)
 		}
 
+		dec_err := json.Unmarshal([]byte(output), &collectors_data)
+		if dec_err != nil {
+			fmt.Println("Ошибка декодирования JSON:", dec_err)
+			return
+		}
+
+		UpdateDeviceNodes(&collectors_data)
+
 		fmt.Println(string(output))
 
-		time.Sleep(time.Second * time.Duration(config.TimeOut))
+		time.Sleep(time.Second * time.Duration(config.UpdateTime))
 	}
 }
--- server_controller.go
+++ server_controller.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
 
 	"github.com/gopcua/opcua/debug"
@@ -29,8 +30,49 @@ var (
 	gencert  = flag.Bool("gen-cert", false, "Generate a new certificate")
 )
 
-var options []server.Option
 var _server *server.Server
+var _node_ns *server.NodeNameSpace
+
+type ModeNodes struct {
+	Mode       *server.Node
+	RunState   *server.Node
+	Status     *server.Node
+	Shutdowns  *server.Node
+	HightSpeed *server.Node
+	AxisMotion *server.Node
+	Mstb       *server.Node
+	LoadExcess *server.Node
+}
+
+type ProgramNodes struct {
+	Frame          *server.Node
+	MainProgNumber *server.Node
+	SubProgNumber  *server.Node
+	PartsCount     *server.Node
+	ToolNumber     *server.Node
+	FrameNumber    *server.Node
+}
+
+type AxisNodes struct {
+	FeedRate           *server.Node
+	FeedOverride       *server.Node
+	JogOverride        *server.Node
+	JogSpeed           *server.Node
+	CurrentLoad        *server.Node
+	CurrentLoadPercent *server.Node
+	ServoLoads         *server.Node
+}
+
+type CollectorNodes struct {
+	address    *server.Node
+	port       *server.Node
+	series     *server.Node
+	mode_nodes ModeNodes
+	prog_nodes ProgramNodes
+	axis_nodes AxisNodes
+}
+
+var col_nodes []CollectorNodes
 
 type Logger int
 
@@ -126,66 +168,184 @@ func inicialize() {
 		}
 	}
 
-	options = opts
-}
-
-func launch() {
-	// Start the server
-	_server = server.New(options...)
+	_server = server.New(opts...)
 	root_ns, _ := _server.Namespace(0)
 	root_obj_node := root_ns.Objects()
 
+	nodeNS := server.NewNodeNameSpace(_server, "Fanuc Devices")
+	nodeNS.Objects().SetDescription("Fanuc devices data", "Data from fanuc collector")
+	_node_ns = nodeNS
+	log.Printf("Node Namespace added at index %d", nodeNS.ID())
+
+	nns_obj := nodeNS.Objects()
+	root_obj_node.AddRef(nns_obj, id.HasComponent, true)
+
+	CreateCollectorNodes(collectors_data, nodeNS)
+}
+
+func AddVariableNode(node_ns *server.NodeNameSpace, node *server.Node, name string, value any) *server.Node {
+	variable := node_ns.AddNewVariableNode(name, value)
+	variable.SetDescription("Description", name)
+	node.AddRef(variable, id.HasComponent, true)
+	return variable
+}
+
+func GetFolderNode(node_id uint16, key string, name string) *server.Node {
+	folder_id := ua.NewStringNodeID(node_id, key)
+	attributes := map[ua.AttributeID]*ua.DataValue{
+		ua.AttributeIDNodeClass:  server.DataValueFromValue(uint32(ua.NodeClassObject)),
+		ua.AttributeIDBrowseName: server.DataValueFromValue(attrs.BrowseName(name)),
+	}
+	folder := server.NewNode(folder_id, attributes, nil, nil)
+	folder.SetDescription("Description", "ru")
+	return folder
+}
+
+func UpdateValue(node *server.Node, value any) {
+	val := ua.DataValue{
+		Value:           ua.MustVariant(value),
+		SourceTimestamp: time.Now(),
+		EncodingMask:    ua.DataValueValue | ua.DataValueSourceTimestamp,
+	}
+	node.SetAttribute(ua.AttributeIDValue, &val)
+	_node_ns.ChangeNotification(node.ID())
+}
+
+func UpdateDeviceNodes(col_data *CollectorsData) {
+	for index, value := range col_data.Collectors {
+		//device data
+		UpdateValue(col_nodes[index].address, string(value.Device.Address))
+		UpdateValue(col_nodes[index].port, int64(value.Device.Port))
+		UpdateValue(col_nodes[index].series, string(value.Device.Series))
+		//mode data
+		UpdateValue(col_nodes[index].mode_nodes.Mode, string(value.Mode.Mode))
+		UpdateValue(col_nodes[index].mode_nodes.RunState, string(value.Mode.RunState))
+		UpdateValue(col_nodes[index].mode_nodes.Status, string(value.Mode.Status))
+		UpdateValue(col_nodes[index].mode_nodes.Shutdowns, string(value.Mode.Shutdowns))
+		UpdateValue(col_nodes[index].mode_nodes.HightSpeed, string(value.Mode.HightSpeed))
+		UpdateValue(col_nodes[index].mode_nodes.AxisMotion, string(value.Mode.AxisMotion))
+		UpdateValue(col_nodes[index].mode_nodes.Mstb, string(value.Mode.LoadExcess))
+		UpdateValue(col_nodes[index].mode_nodes.LoadExcess, string(value.Mode.LoadExcess))
+		//program data
+		UpdateValue(col_nodes[index].prog_nodes.Frame, string(value.Program.Frame))
+		UpdateValue(col_nodes[index].prog_nodes.MainProgNumber, int64(value.Program.MainProgNumber))
+		UpdateValue(col_nodes[index].prog_nodes.SubProgNumber, int64(value.Program.SubProgNumber))
+		UpdateValue(col_nodes[index].prog_nodes.PartsCount, int64(value.Program.PartsCount))
+		UpdateValue(col_nodes[index].prog_nodes.ToolNumber, int64(value.Program.ToolNumber))
+		UpdateValue(col_nodes[index].prog_nodes.FrameNumber, int64(value.Program.FrameNumber))
+		//axis data
+		UpdateValue(col_nodes[index].axis_nodes.FeedRate, int64(value.Axes.FeedRate))
+		UpdateValue(col_nodes[index].axis_nodes.FeedOverride, int64(value.Axes.FeedOverride))
+		UpdateValue(col_nodes[index].axis_nodes.JogOverride, float64(value.Axes.JogOverride))
+		UpdateValue(col_nodes[index].axis_nodes.JogSpeed, int64(value.Axes.JogSpeed))
+		UpdateValue(col_nodes[index].axis_nodes.CurrentLoad, float64(value.Axes.CurrentLoad))
+		UpdateValue(col_nodes[index].axis_nodes.CurrentLoadPercent, float64(value.Axes.CurrentLoadPercent))
+	}
+}
+
+func CreateCollectorNodes(data CollectorsData, node_ns *server.NodeNameSpace) {
+	node_obj := node_ns.Objects()
+	collectors := data.Collectors
+	for index := range collectors {
+		var str_index = strconv.Itoa(index)
+		var col_ns CollectorNodes
+		device_folder := GetFolderNode(node_ns.ID(), "device "+str_index, "Device "+str_index)
+		node_ns.AddNode(device_folder)
+		node_obj.AddRef(device_folder, id.HasComponent, true)
+
+		address_val := string(collectors[index].Device.Address)
+		col_ns.address = AddVariableNode(node_ns, device_folder, "address", address_val)
+
+		port_val := int64(collectors[index].Device.Port)
+		col_ns.port = AddVariableNode(node_ns, device_folder, "port", port_val)
+
+		series_val := string(collectors[index].Device.Series)
+		col_ns.series = AddVariableNode(node_ns, device_folder, "series", series_val)
+
+		//mode data folder + variables
+		mode_folder := GetFolderNode(node_ns.ID(), "mode_data ", "Mode Data ")
+		node_ns.AddNode(mode_folder)
+		device_folder.AddRef(mode_folder, id.HasComponent, true)
+
+		mode_val := collectors[index].Mode.Mode
+		col_ns.mode_nodes.Mode = AddVariableNode(node_ns, mode_folder, "mode", mode_val)
+
+		run_state_val := collectors[index].Mode.RunState
+		col_ns.mode_nodes.RunState = AddVariableNode(node_ns, mode_folder, "run_state", run_state_val)
+
+		status_val := collectors[index].Mode.Status
+		col_ns.mode_nodes.Status = AddVariableNode(node_ns, mode_folder, "status", status_val)
+
+		shutdowns_val := collectors[index].Mode.Shutdowns
+		col_ns.mode_nodes.Shutdowns = AddVariableNode(node_ns, mode_folder, "shutdowns", shutdowns_val)
+
+		hight_speed_val := collectors[index].Mode.HightSpeed
+		col_ns.mode_nodes.HightSpeed = AddVariableNode(node_ns, mode_folder, "hight_speed", hight_speed_val)
+
+		axis_motion_val := collectors[index].Mode.AxisMotion
+		col_ns.mode_nodes.AxisMotion = AddVariableNode(node_ns, mode_folder, "axis_motion", axis_motion_val)
+
+		mstb_val := collectors[index].Mode.Mstb
+		col_ns.mode_nodes.Mstb = AddVariableNode(node_ns, mode_folder, "mstb", mstb_val)
+
+		load_excess_val := collectors[index].Mode.LoadExcess
+		col_ns.mode_nodes.LoadExcess = AddVariableNode(node_ns, mode_folder, "load_excess", load_excess_val)
+
+		//program data folder + variables
+		program_folder := GetFolderNode(node_ns.ID(), "program_data", "Program Data")
+		node_ns.AddNode(program_folder)
+		device_folder.AddRef(program_folder, id.HasComponent, true)
+
+		frame_val := string(collectors[index].Program.Frame)
+		col_ns.prog_nodes.Frame = AddVariableNode(node_ns, program_folder, "frame", frame_val)
+
+		main_prog_number_val := int64(collectors[index].Program.MainProgNumber)
+		col_ns.prog_nodes.MainProgNumber = AddVariableNode(node_ns, program_folder, "main_prog_number", main_prog_number_val)
+
+		sub_prog_number_val := int64(collectors[index].Program.SubProgNumber)
+		col_ns.prog_nodes.SubProgNumber = AddVariableNode(node_ns, program_folder, "sub_prog_number", sub_prog_number_val)
+
+		parts_count_val := int64(collectors[index].Program.PartsCount)
+		col_ns.prog_nodes.PartsCount = AddVariableNode(node_ns, program_folder, "parts_count", parts_count_val)
+
+		tool_number_val := int64(collectors[index].Program.ToolNumber)
+		col_ns.prog_nodes.ToolNumber = AddVariableNode(node_ns, program_folder, "tool_number", tool_number_val)
+
+		frame_number_val := int64(collectors[index].Program.FrameNumber)
+		col_ns.prog_nodes.FrameNumber = AddVariableNode(node_ns, program_folder, "frame_number", frame_number_val)
+
+		//axes data folder + variables
+		axes_folder := GetFolderNode(node_ns.ID(), "axes_data", "Axes Data")
+		node_ns.AddNode(axes_folder)
+		device_folder.AddRef(axes_folder, id.HasComponent, true)
+
+		feedrate_val := int64(collectors[index].Axes.FeedRate)
+		col_ns.axis_nodes.FeedRate = AddVariableNode(node_ns, axes_folder, "feedrate", feedrate_val)
+
+		feed_override_val := int64(collectors[index].Axes.FeedOverride)
+		col_ns.axis_nodes.FeedOverride = AddVariableNode(node_ns, axes_folder, "feed_override", feed_override_val)
+
+		jog_override_val := float64(collectors[index].Axes.JogOverride)
+		col_ns.axis_nodes.JogOverride = AddVariableNode(node_ns, axes_folder, "jog_override", jog_override_val)
+
+		jog_speed_val := int64(collectors[index].Axes.JogSpeed)
+		col_ns.axis_nodes.JogSpeed = AddVariableNode(node_ns, axes_folder, "jog_speed", jog_speed_val)
+
+		current_load_val := float64(collectors[index].Axes.CurrentLoad)
+		col_ns.axis_nodes.CurrentLoad = AddVariableNode(node_ns, axes_folder, "current_load", current_load_val)
+
+		current_load_percent_val := float64(collectors[index].Axes.CurrentLoadPercent)
+		col_ns.axis_nodes.CurrentLoadPercent = AddVariableNode(node_ns, axes_folder, "current_load_percent", current_load_percent_val)
+
+		col_nodes = append(col_nodes, col_ns)
+	}
+}
+
+func start() {
 	if err := _server.Start(context.Background()); err != nil {
 		log.Fatalf("Error starting server, exiting: %s", err)
 	}
 	defer _server.Close()
-
-	nodeNS := server.NewNodeNameSpace(_server, "NodeNamespace")
-	log.Printf("Node Namespace added at index %d", nodeNS.ID())
-	nns_obj := nodeNS.Objects()
-	root_obj_node.AddRef(nns_obj, id.HasComponent, true)
-
-	var1 := nodeNS.AddNewVariableNode("TestVar1", float32(123.45))
-	nns_obj.AddRef(var1, id.HasComponent, true)
-
-	var2Value := "BOBER"
-	var2 := nodeNS.AddNewVariableStringNode("TestVar2", var2Value)
-	nns_obj.AddRef(var2, id.HasComponent, true)
-
-	var3 := server.NewNode(
-		ua.NewNumericNodeID(nodeNS.ID(), 12345),
-		map[ua.AttributeID]*ua.DataValue{
-			ua.AttributeIDBrowseName: server.DataValueFromValue(attrs.BrowseName("MyBrowseName")),
-			ua.AttributeIDNodeClass:  server.DataValueFromValue(uint32(ua.NodeClassVariable)),
-		},
-		nil,
-		func() *ua.DataValue { return server.DataValueFromValue(12.34) },
-	)
-	nodeNS.AddNode(var3)
-	nns_obj.AddRef(var3, id.HasComponent, true)
-
-	// simulate a background process updating the data in the namespace.
-	go func() {
-		updates := 0
-		num := 42
-		time.Sleep(time.Second * 10)
-		for {
-			updates++
-			num++
-			last_value := var1.Value().Value.Value().(float32)
-			last_value += 1
-			val := ua.DataValue{
-				Value:           ua.MustVariant(last_value),
-				SourceTimestamp: time.Now(),
-				EncodingMask:    ua.DataValueValue | ua.DataValueSourceTimestamp,
-			}
-			var1.SetAttribute(ua.AttributeIDValue, &val)
-
-			nodeNS.ChangeNotification(var1.ID())
-
-			time.Sleep(time.Second)
-		}
-	}()
 
 	sigch := make(chan os.Signal, 1)
 	signal.Notify(sigch, os.Interrupt)
